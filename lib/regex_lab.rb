4a563d678cc427a782901139cf26840e67cc6806
Clayon Stewart <c.stewart89@gmail.com>
2021-03-05 14:56:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,15 @@
 def starts_with_a_vowel?(word)
-  word.match(/\b[aeiouAEIOU]/)
+  matched = word.match(/\b[aeiouAEIOU]/)
+  
+  if matched
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  matched = text.scan()
 end
 
 def words_five_letters_long(text)
