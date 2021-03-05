11215d8aa5e73bcaf3c5aa83945292e86a9be7a2
Clayon Stewart <c.stewart89@gmail.com>
2021-03-05 16:30:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,7 +27,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  matches = phone.scan(/\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}/)
+  matches = phone.scan(/\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{4}\z/)
   puts matches
   if matches
     return true
