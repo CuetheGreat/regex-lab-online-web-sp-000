7e0ae6143e4783ccce6bc3961663d77ac7cd64f2
Clayon Stewart <c.stewart89@gmail.com>
2021-03-05 15:50:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,5 +27,11 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/\d{10}|[(]\d{3}[)]\d{3}.\d{4}|\d{3}.\d{3}.\d{4}|[(]\d{3}[)]\d{7}/)
+  
+  if phone
+    return true
+  else
+    return false
+  end
 end
